444a1ab476ad0aa792099f52a46cd80b53c85630
Daan Schrijver <daanschrijver040@gmail.com>
2023-11-05 22:48:20 +0100

    Ignore .iso and .bin

--- kernel/big32.asm
+++ kernel/big32.asm
@@ -51,3 +51,50 @@ rand_big32:
     pop ecx
     pop eax
     ret
+
+
+mul_two_big32:
+    ; Multiplies two big32's with the same amount of 
+    ; 32-bit limbs. The result is stored in a bigint of
+    ; twice amount of 32-bit limbs. The function expects
+    ; The output big32 to be zero initially. 
+    ;
+    ; Parameters
+    ; ----------
+    ; EBX: Number of 32-bit limbs in each input big32
+    ; ESI: Pointer to 1st input big32
+    ; EDI: Pointer to 2nd input big32
+    ; EDX: Pointer to output big32
+
+    pushad
+    push ebp
+    mov ebp, esp
+
+    mov dword [ebp - 4], ebx
+    mov dword [ebp - 8], esi
+    mov dword [ebp - 12], edi
+    mov dword [ebp - 16], edx
+    sub esp, 16
+
+    mov ebx, 0
+    .loop1:
+        mov ecx, 0
+        .loop2:
+            mov eax, dword [esi + 4*ebx]
+            mov edx, dword [edi + 4*ecx]
+            mul edx
+
+            ; TODO!
+
+            inc ecx
+            cmp ebx, dword [ebp - 4]
+            jne .loop2
+            
+        inc ebx
+        cmp ebx, dword [ebp - 4]
+        jne .loop1
+
+    mov esp, ebp
+    pop ebp
+    popad
+    ret
